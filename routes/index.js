190e21431cf6ef1553bbb3d33d25bbdbd6bf98cc
ran <snklvm36@gmail.com>
2025-11-18 11:15:59 +0400

    视图模板已经开发完成

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+// 记账本的列表
+router.get('/account', function(req, res, next) {
+  res.render('list');
+});
+
+// 显示添加记录的视图
+router.get('/account/create', function (req, res, next) {
+  res.render('create');
+});
+
+module.exports = router;
