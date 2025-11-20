4fb12bb8550c90274cefd5cab6fc0284e751b4fb
ran <snklvm36@gmail.com>
2025-11-20 11:27:59 +0400

    页面初始化完成

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 // 记账本的列表
 router.get('/account', function(req, res, next) {
-  res.render('list');
+  res.render('../views/index.ejs');
 });
 
 // 显示添加记录的视图
 router.get('/account/create', function (req, res, next) {
-  res.render('create');
+  res.render('../views/create.ejs');
 });
 
 module.exports = router;
